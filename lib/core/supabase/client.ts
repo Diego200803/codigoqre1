655e0ba9504c66172b195b1b09dbbe401d059244
matcabreraqui <matcabreraqui@gmail.com>
2026-03-11 20:10:25 +0000

    🐛Fix: Se arreglaron algunos errores del cliente de supabase

--- lib/core/supabase/client.ts
+++ lib/core/supabase/client.ts
@@ -1,14 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
 
 const SUPABASE_URL = 'https://wbvncfdfbsmbsgyyaxan.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indidm5jZmRmYnNtYnNneXlheGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NTI3OTEsImV4cCI6MjA4ODIyODc5MX0.Pa9qB_Km66f-cXPBZtEwZeZnzW72QIKb4hXkEP9h4PQ'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indidm5jZmRmYnNtYnNneXlheGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NTI3OTEsImV4cCI6MjA4ODIyODc5MX0.Pa9qB_Km66f-cXPBZtEwZeZnzW72QIKb4hXkEP9h4PQ';
+
+const storage = Platform.OS === 'web' ? undefined : AsyncStorage;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    storage:          AsyncStorage,  // persiste sesión en el dispositivo
-    autoRefreshToken: true,          // renueva el token automáticamente
-    persistSession:   true,          // mantiene sesión aunque cierres la app
-    detectSessionInUrl: false,       // necesario en React Native (no hay URLs)
+    storage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
   },
 });
